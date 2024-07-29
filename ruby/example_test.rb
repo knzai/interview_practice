80d8648ba435e2095403d4adb0dbec1b5a9b45fe
Kenzi Connor <git@knz.ai>
2024-07-29 12:58:26 -0700

    just use test/unit instead

--- ruby/example_test.rb
+++ ruby/example_test.rb
@@ -1,7 +1,8 @@
-require "minitest/autorun"
+
+require 'test/unit'
 require "./example"
 
-class MyTest < Minitest::Test
+class MyTest < Test::Unit::TestCase
   def test_my_function
       assert_equal(1, my_function)
   end
