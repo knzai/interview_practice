c47250c58584bea712cd8839c65554d474c27563
Kenzi Connor <git@knz.ai>
2024-08-18 12:08:11 -0700

    get rake working right with ruby 3

--- ruby/example.rb
+++ ruby/example.rb
@@ -2,20 +2,15 @@
 # frozen_string_literal: true
 
 require 'optparse'
-Options = Struct.new(:name)
 
 # rubocop:disable Metrics/MethodLength
 # Ruby's default CLI flag and option parsing
 class Parser
   def self.parse(options)
-    args = Options.new('world')
+    args = {}
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = 'Usage: example.rb [options]'
-
-      opts.on('-nNAME', '--name=NAME', 'Name to say hello to') do |n|
-        args.name = n
-      end
 
       opts.on('-h', '--help', 'Prints this help') do
         puts opts
@@ -31,7 +26,7 @@ end
 
 def my_function
   options = Parser.parse ARGV
-  puts options.name
+  puts options
 
   0
 end
